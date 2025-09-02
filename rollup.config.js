97ab1cd9e4c8eadb5e08aa83f4a33596ca991de0
maguwebdev <shyrokykhivan@gmail.com>
2025-09-03 00:12:31 +0300

    Fixed rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,66 @@
 import resolve from '@rollup/plugin-node-resolve';
+import {defineConfig} from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { readFileSync } from 'fs';
 
 const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
 
-export default {
+export default defineConfig([
+  {
     input: 'src/index.ts',
-    output: [
-        {
-            file: packageJson.main,
-            format: 'cjs',
-            sourcemap: true,
-        },
-        {
-            file: packageJson.module,
-            format: 'esm',
-            sourcemap: true,
-        },
-    ],
+    output: {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named'
+    },
     plugins: [
-        resolve(),
-        commonjs(),
-        typescript({ 
-            tsconfig: './tsconfig.json',
-            declaration: true,
-            declarationDir: './dist'
-        }),
+      resolve({
+        preferBuiltins: false,
+        extensions: ['.js', '.ts']
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: false
+      }),
     ],
-    external: ['lodash', 'lodash/toPath'],
-};
+    external: []
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true
+    },
+    plugins: [
+      resolve({
+        preferBuiltins: false,
+        extensions: ['.js', '.ts']
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: false
+      }),
+    ],
+    external: []
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: packageJson.types,
+      format: 'esm'
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: './dist',
+        emitDeclarationOnly: true
+      })
+    ]
+  }
+]);
